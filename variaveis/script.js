d142afe066ce960c5d6dec91dbec9759ff293e04
Erycky <48848271+Erycky@users.noreply.github.com>
2026-06-26 15:07:19 -0300

    :sparkles: feat: Variáveis

---
+++ variaveis/script.js
@@ -0,0 +1,19 @@
+// Declarar variável com um nome
+var nome = "Jorge";
+
+// Declarar variável com idade
+var idade = "20";
+
+// Declarar uma variaável com uma comida
+// famosa e não atribuir valor
+var comidaFamosa;
+
+// Atribuir valor a comida famosa
+var comidaFamosa = "Churrasco";
+
+// Declarar 5 variáveis diferentes sem valores
+var estado;
+var cidade;
+var rua;
+var cep;
+var casa;
