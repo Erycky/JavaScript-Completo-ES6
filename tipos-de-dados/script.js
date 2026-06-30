50c508a2802daa74f49c834f6c2396f2fd424251
Erycky <48848271+Erycky@users.noreply.github.com>
2026-06-30 09:54:22 -0300

    :sparkles: feat: Tipos de dados

---
+++ tipos-de-dados/script.js
@@ -0,0 +1,21 @@
+// Declare uma variável contendo uma string
+var nome = "Jorge";
+
+// Declare uma variável contendo um número dentro de uma string
+var idade = "20";
+
+// Declare uma varável com sua idade
+var minhaIdade = 30;
+
+// Declare duas variáveis, uma com seu nome
+// e outra com seu sobrenome e some as mesmas
+var primeiroNome = "Jorge";
+var segundoNome = "Pescador";
+
+var nomeCompleto = (`${primeiroNome} ${segundoNome}`);
+
+// Coloque a seguinte frase em uma variável: Its's time
+var frase = "It's time";
+
+// Verifique o tipo da variável que contém o seu nome
+var tipo = (typeof nome);
